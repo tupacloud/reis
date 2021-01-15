1e0da28d898adbf42b50a22c406a7d20fa5b8ce8
Rafael <rrleme@gmail.com>
2021-01-15 17:12:40 -0300

    primeiras linhas de código

--- modulo/reis_esp32/reis_esp32.ino
+++ modulo/reis_esp32/reis_esp32.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include "header.h"
+
+void setup() {
+
+  Serial.begin(115200);
+
+  xTaskCreatePinnedToCore(
+                    loopCoreZero,   /* função que implementa a tarefa */
+                    "loopCoreZero",  /* nome da tarefa */
+                    10000,         /* número de palavras a serem alocadas para uso com a pilha da tarefa */
+                    NULL,          /* parâmetro de entrada para a tarefa (pode ser NULL) */
+                    1,             /* prioridade da tarefa (0 a N) */
+                    NULL,          /* referência para a tarefa (pode ser NULL) */
+                    taskCoreZero);   /* Núcleo que executará a tarefa */
+  delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+void loopCoreZero(void * pvParameters ){
+  
+  while(true){
+    
+    delay(1);
+  }
+  
 }
